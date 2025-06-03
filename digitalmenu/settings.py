6b257bbb91eb7cc2f088271fd612d9bd63013134
Your Name <you@example.com>
2025-06-04 00:29:27 +0300

    render commit

--- digitalmenu/settings.py
+++ digitalmenu/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)lh%xs3^mpg#m0jzk8mmi7^ndfohsdtkj8hd_t+en-!glhzr8b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'custom-digitalmenu.onrender.com', 'localhost', '127.0.0.1' ]
 
 
 # Application definition
